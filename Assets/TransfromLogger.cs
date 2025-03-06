3ad89388625a59ce0fbc50d181be0b7b1fb53d22
Carlos Cortes <ccs@gti.ssr.upm.es>
2025-03-06 10:22:57 +0100

    I have added a Script for recording the position of the hands joints every 500 ms

---
+++ Assets/TransfromLogger.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using Oculus.Interaction;
+public class TransformLogger : MonoBehaviour
+{
+    public HandVisual RightHand;
+    public HandVisual LeftHand;
+    private string filePath;
+    private Coroutine loggingCoroutine;
+
+    void Start()
+    {
+        long epochTime = new System.DateTimeOffset(System.DateTime.UtcNow).ToUnixTimeSeconds();
+        filePath = Path.Combine(Application.persistentDataPath, $"TransformLog_{epochTime}.txt");
+        loggingCoroutine = StartCoroutine(LogTransforms());
+    }
+
+    IEnumerator LogTransforms()
+    {
+        using (StreamWriter writer = new StreamWriter(filePath, false))
+        {
+            while (true)
+            {
+                string Time = new System.DateTimeOffset(System.DateTime.UtcNow).ToUnixTimeSeconds().ToString();
+                foreach (Transform t in RightHand.Joints)
+                {
+                    writer.WriteLine(Time + ", RightHand," + t.name + ", " + t.position);
+                }
+                writer.Flush(); // Ensure data is written to the file
+                foreach (Transform t in LeftHand.Joints)
+                {
+                    writer.WriteLine(Time + ", LeftHand," + t.name + ", " + t.position);
+                }
+                writer.Flush(); // Ensure data is written to the file
+                yield return new WaitForSeconds(0.5f);
+            }
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (loggingCoroutine != null)
+        {
+            StopCoroutine(loggingCoroutine);
+        }
+    }
+}
+
